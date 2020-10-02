bb639c22611ffa146ab8929945e9c95e02ab97d3
Ben Cosgrave <cosgraveben@gmail.com>
2020-10-02 18:11:32 +0100

    lab 1 completed

--- Game Engines Examples/Assets/ColorShader.shader
+++ Game Engines Examples/Assets/ColorShader.shader
@@ -67,8 +67,9 @@ Shader "Custom/ColorShader" {
 			// and some functions you can use sqrt, pow, abs. 
 			// And the modulus operator %
 			// You can also use the built in variable _Time 
-			// that gives the time in seconds since the program started. It's a float.			
-			float hue = 1;
+			// that gives the time in seconds since the program started. It's a float.
+			float dist = sqrt(pow(IN.worldPos.x, 2) + pow(IN.worldPos.y, 2));
+			float hue = abs(((dist / 10.0f) - _Time)) % 1.0;
 			fixed3 c = hsv_to_rgb(float3(hue, 1, 1));
 			o.Albedo = c.rgb;
 			// Metallic and smoothness come from slider variables
---
+++ Game Engines Examples/Assets/Generator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public int rings = 1;
+    public GameObject ringPrefab;
+
+    public float radius = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int j = 0; j < rings; j++)
+        {
+            // Calculate how many fit in the cirumference of the circle
+            float circumference = Mathf.PI * 2.0f * radius;
+            float elements = (int)circumference;
+
+            float theta = Mathf.PI * 2.0f / (float)elements;
+            for (int i = 0; i < elements; i++)
+            {
+                GameObject hex = GameObject.Instantiate<GameObject>(ringPrefab);
+                Vector3 pos = new Vector3(Mathf.Sin(theta * i) * radius, 0, Mathf.Cos(theta * i) * radius);
+                hex.transform.position = transform.TransformPoint(pos);
+                hex.GetComponent<Renderer>().material.color = Color.HSVToRGB((float) j / rings, 1, 1);
+                hex.transform.parent = this.transform;
+            }
+            radius += 1.2f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
